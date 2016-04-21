8ec37b223c26b5dda1a8f5503036c9aa8084633f
Kasper Piskorski <kasper@mindmaps.io>
2016-04-21 16:03:12 +0100

    removed unused imports

--- src/main/java/io/mindmaps/reasoner/topology/MindmapsReasonerTopology.java
+++ src/main/java/io/mindmaps/reasoner/topology/MindmapsReasonerTopology.java
@@ -5,28 +5,8 @@ import backtype.storm.StormSubmitter;
 import backtype.storm.generated.StormTopology;
 import backtype.storm.spout.SchemeAsMultiScheme;
 import backtype.storm.topology.TopologyBuilder;
-import backtype.storm.tuple.Fields;
 
-import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.apache.kafka.clients.consumer.ConsumerRecords;
-import org.apache.kafka.clients.consumer.KafkaConsumer;
-import org.apache.kafka.clients.producer.KafkaProducer;
-import org.apache.kafka.clients.producer.Producer;
-import org.apache.kafka.clients.producer.ProducerRecord;
-
-import org.apache.kafka.common.PartitionInfo;
-import org.apache.kafka.common.TopicPartition;
-import org.apache.kafka.common.serialization.StringDeserializer;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import storm.kafka.*;
-
-
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
-import java.util.Properties;
 
 import io.mindmaps.reasoner.topology.bolt.*;
 
